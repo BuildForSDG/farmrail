f83bc4e9ac5e823b3d218611ecd78a1def82e4c0
Maria Angella <maria5angella@gmail.com>
2020-05-09 10:42:22 +0300

    ft-routes-web-ui

--- src/web_ui/src/App.test.js
+++ src/web_ui/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { Router } from 'react-router';
+import { localRoutes } from "./store/index";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Homepage', () => {
+    <Router >
+    <App />
+  </Router> ,
+
+
+    expect(localRoutes.homepage).toBe("/");
 });
--- src/web_ui/src/components/AppRoutes/index.test.js
+++ src/web_ui/src/components/AppRoutes/index.test.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { Router } from 'react-router';
-import Homepage from '../Homepage';
-import Dashboard from "../Dashboard";
-import Signup from "../Forms/Signup";
-import Login from "../Forms/Login";
 import ContentSwitch from './index';
-
+import { localRoutes } from "../../store/index";
 
 
 
 
 test("redirects to specified page", () => {
-  
-  render(
-    <Router >
-      <ContentSwitch />
-    </Router>,
-    node
-  );
-  expect(Homepage.location.pathname).toBe("/");
-  expect(Dashboard.location.pathname).toBe("/dashboard");
-  expect(Signup.location.pathname).toBe("/signup");
-  expect(Login.location.pathname).toBe("/login");
- 
+
+
+  <Router >
+    <ContentSwitch />
+  </Router> ,
+
+
+    expect(localRoutes.dashboard).toBe("/dashboard");
+  expect(localRoutes.signup).toBe("/signup");
+  expect(localRoutes.login).toBe("/login");
+
 });
 
