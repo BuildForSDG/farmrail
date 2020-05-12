4207edbcad497a27c58a6a70810ed98a17753608
PaulRuby <44568805+LadPaule@users.noreply.github.com>
2020-05-12 05:10:52 +0300

    update

--- src/services/produce_api/produceAPI/produce/models.py
+++ src/services/produce_api/produceAPI/produce/models.py
@@ -5,7 +5,7 @@ class Produce(models.Model):
     thumbnail = models.ImageField(null=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=3, max_digits=10, default= 00.0)
-	created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
