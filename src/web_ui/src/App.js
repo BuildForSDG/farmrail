ace6c06054f38441b318f7bd3cb787b63669c63b
kmwamasali <kmwamasali@gmail.com>
2020-05-13 19:35:22 +0300

    refactor:  add linting to react project
    Linting for the project repo is setup plus unit test files and neccesary dependecies added.
    - eslint
    - eseslint-config-airbnb
    - eslint-config-prettier
    - eslint-plugin-import
    - eslint-plugin-jsx-a11y
    - eslint-plugin-prettier
    - eslint-plugin-react
    - eslint-plugin-react-hooks
    - prettier
    
    fixes #32 #41

--- src/web_ui/src/App.js
+++ src/web_ui/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Homepage from "./components/Homepage";
-import "./App.css";
-import ContentSwitch from "./components/AppRoutes/index";
-
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import './App.css';
+import ContentSwitch from './components/AppRoutes/index';
 
 export default function App() {
   return (
@@ -11,14 +9,8 @@ export default function App() {
       <div className="App">
         <Switch>
           <ContentSwitch />
-          <Route path="/">
-            <Homepage />
-          </Route>
-
         </Switch>
       </div>
     </Router>
   );
 }
-
-
--- src/web_ui/src/components/AppRoutes/index.js
+++ src/web_ui/src/components/AppRoutes/index.js
@@ -1,29 +1,22 @@
-import React from "react"
-import {Link, Route, Switch} from 'react-router-dom'
-import {localRoutes} from "../../store/index";
-import Layout from "../Layout";
-import Homepage from "../Homepage";
-import Signup from "../Forms/Signup";
-import Login from "../Forms/Login";
-
-
-
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import localRoutes from './routes';
+import Homepage from '../Pages/Homepage';
+import Dashboard from '../Pages/Dashboard';
+import Signup from '../Forms/Signup/Signup';
+import Login from '../Forms/Login/Login';
+import NoMatch from '../Pages/NoMatch';
 
 const ContentSwitch = () => {
-    return <Switch>
-        <Route exact={true} path="/" component={Homepage}/>
-        <Route path={localRoutes.homepage} component={Homepage}/>
-        <Route path={localRoutes.signup} component={Signup}/>
-        <Route path={localRoutes.login} component={Login}/>
-        <Route component={NoMatch}/>
+  return (
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route path={localRoutes.dashboard} component={Dashboard} />
+      <Route path={localRoutes.signup} component={Signup} />
+      <Route path={localRoutes.login} component={Login} />
+      <Route component={NoMatch} />
     </Switch>
-}
+  );
+};
 
-const NoMatch = () => (
-    <Layout>
-        <h2>Oops nothing here!!</h2>
-        <Link to="/">Take me home</Link>
-    </Layout>
-)
-
-export default ContentSwitch
+export default ContentSwitch;
