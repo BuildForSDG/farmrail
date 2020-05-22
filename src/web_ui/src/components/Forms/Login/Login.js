d66f70ca2bb534cdbb1ff05905e1fa7b37ad3623
Maria Angella <maria5angella@gmail.com>
2020-05-22 14:58:55 +0300

    ft-login-web-ui

--- src/web_ui/src/components/Forms/Login/Login.js
+++ src/web_ui/src/components/Forms/Login/Login.js
@@ -1,35 +1,120 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+
+
+// export default function Login() {
+//   return (
+//     <div>
+      
+//       <Link to="/dashboard">
+//         <h2>Login</h2>
+//       </Link>
+//     </div>
+//   );
+// }
+
+
+
+
 import React from 'react';
+import {Button} from "@material-ui/core";
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import {Form, Formik, } from 'formik';
+import {useDispatch} from 'react-redux'
+import {handleLogin} from "../../Layout/LayoutDashboard/coreActions";
+
+import * as yup from "yup";
+// import {post} from "../../utils/ajax";
+// import {remoteRoutes} from "../../data/constants";
+// import Toast from "../../utils/Toast";
+import XTextInput from "./XTextInput";
+import {useLoginStyles} from "./loginStyles";
 import { Link } from 'react-router-dom';
 
+function Login() {
+    const classes = useLoginStyles();
+    // const dispatch = useDispatch();
+    // const onSubmit = (data, actions) => {
+    //     // post(remoteRoutes.login, data, resp => {
+    //     //     dispatch(handleLogin(resp))
+    //     // }, () => {
+    //     //     Toast.error("Invalid username/password")
+    //     // }, () => {
+    //     //     actions.setSubmitting(false)
+    //     // })
+    // }
 
+    return (
+        <main className={classes.main}>
+            <CssBaseline/>
+            <Paper className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockIcon/>
+                </Avatar>
+                <Typography component="h1">
+                    Sign in
+                </Typography>
+                <Formik
+                    initialValues={{
+                        "username": "maria5angella@gmail.com",
+                        "password": "Farmrail@123"
+                    }}
+                    // validationSchema={schema}
+                    // onSubmit={onSubmit}
+                >
+                    {(formState) => (
+                        <Form className={classes.form}>
+                            <XTextInput
+                                type='email'
+                                name='username'
+                                label='Email'
+                                autoComplete="off"
+                                autoFocus
+                                margin="normal"
+                            />
 
-export default function Login() {
-  return (
-    <div>
-      
-      <Link to="/dashboard">
-        <h2>Login</h2>
-      </Link>
-    </div>
-  );
+                            <XTextInput
+                                type='password'
+                                name='password'
+                                label='Password'
+                                autoComplete="off"
+                                margin="normal"
+                            />
+                            <Link to="/dashboard">
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                                disabled={formState.isSubmitting}
+                            >
+                                Sign in
+                                
+
+                            </Button>
+                            </Link>
+                        </Form>
+                    )}
+                </Formik>
+            </Paper>
+        </main>
+    );
 }
 
-// import { Link } from 'react-router-dom';
-// import React, { PureComponent } from "react"
-// import { Form, Field, ErrorMessage } from "formik"
 
-// export default class Login extends PureComponent {
-//   render() {
-//     return (
-//       <Form>
-//         <Field type="text" name="email" placeholder="email" />
-//         <ErrorMessage name="email" />
-//         <Field type="text" name="password" placeholder="password" />
-//         <ErrorMessage name="password" />
-//         <Link to="/dashboard">
-//         <button type="submit"> Submit </button>
-//         </Link>
-//       </Form>
-//     )
-//   }
-// }
+export const schema = yup.object().shape(
+    {
+        username: yup.string().email('Invalid email').required("Email is required"),
+        password: yup.string().required("Password is required")
+    }
+);
+
+export default Login
+
+
---
+++ src/web_ui/src/components/Forms/Login/inputHelpers.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {isEmpty} from "lodash";
+
+
+
+
+
+export const hasValue = (text) => {
+    return !isEmpty(text)
+}
+
+
+
+
+
+
