49c491cb4695e2ec267477dac66fa86380c0b59d
johnakon <john.akonya@gmail.com>
2020-05-27 13:56:12 +0300

    changes made

--- src/services/membership_api/index.js
+++ src/services/membership_api/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-
 import express from 'express';
 import helloMembers from './routes/memberRoute';
 
 const app = express();
 
 app.use(helloMembers);
+
+require('dotenv').config();
 
 console.log('Listening on port:', process.env.PORT);
