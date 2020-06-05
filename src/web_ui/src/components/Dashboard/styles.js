20f7f2702e244020d29cdae5b925812947765b6c
Maria Angella <maria5angella@gmail.com>
2020-06-05 12:31:48 +0300

    ft-farmitem

--- src/web_ui/src/components/Dashboard/styles.js
+++ src/web_ui/src/components/Dashboard/styles.js
@@ -1,6 +1,6 @@
 import grey from "@material-ui/core/colors/grey";
 import {createStyles, makeStyles, Theme} from "@material-ui/core";
-import {themeBackground} from "./custom-colors";
+import {themeBackground} from "../Layout/LayoutDashboard/custom-colors";
 
 export const drawerWidth = 240;
 export const navBackgroundColor = grey[900];
